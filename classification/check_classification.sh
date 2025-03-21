94e24e14e776c2719b287d270d4bc9501b97891c
chris <christoph-heck@web.de>
2025-03-21 09:20:14 +0100

    update

--- classification/check_classification.sh
+++ classification/check_classification.sh
@@ -5,4 +5,5 @@ python3 check_classification.py --data_dir /home/chris/experiment_data/12_2025_0
 python3 check_classification.py --data_dir /home/chris/experiment_data/12_2025_03_06-2025_03_10 --from_date "2025-03-07 07:00" --until_date "2025-03-09 19:30" --prefix C2 --threshold 0.5 --plant_id 3 # Forgot about Z-score Factor
 python3 check_classification.py --data_dir /home/chris/experiment_data/13_2025_03_11-2025_03_14 --from_date "2025-03-12 07:00" --until_date "2025-03-13 19:30" --prefix C1 --threshold 0.5 --plant_id 3
 python3 check_classification.py --data_dir /home/chris/experiment_data/13_2025_03_11-2025_03_14 --from_date "2025-03-12 07:00" --until_date "2025-03-13 19:30" --prefix C2 --threshold 0.5 --plant_id 3
-
+python3 check_classification.py --data_dir /home/chris/experiment_data/14_2025_03_14-2025_03_21 --from_date "2025-03-14 07:00" --until_date "2025-03-20 19:30" --prefix C1 --threshold 0.5 --plant_id 3
+python3 check_classification.py --data_dir /home/chris/experiment_data/14_2025_03_14-2025_03_21 --from_date "2025-03-14 07:00" --until_date "2025-03-20 19:30" --prefix C2 --threshold 0.5 --plant_id 3
